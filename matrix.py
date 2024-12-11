ccf9b137984deb3f0aa059a088c268d4226778c2
spinythyme34 <spinythyme05@gmail.com>
2024-12-12 01:54:54 +0530

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,16 +1,22 @@
-import numpy as py
-A=py.array([[1,2,3],[4,5,6],[7,8,9]])
-B=py.array([[9,8,7],[6,5,4],[3,2,1]])
-result=py.add(A,B)
-print(result)
+import numpy as np
 
-import numpy as py
-A=py.array([[1,2,3],[4,5,6],[7,8,9]])
-B=py.array([[9,8,7],[6,5,4],[3,2,1]])
-print(A-B)
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
 
-import numpy as py
-A=py.array([[1,2,3],[4,5,6],[7,8,9]])
-B=py.array([[9,8,7],[6,5,4],[3,2,1]])
-result=py.dot(A,B)
-print(result)
+C_add = np.add(A, B)
+C_sub = np.subtract(A, B)
+C_mul = np.multiply(A, B)
+
+C_dot = np.dot(A, B)
+
+print("Matrix Addition:")
+print(C_add)
+
+print("\nMatrix Subtraction:")
+print(C_sub)
+
+print("\nMatrix Multiplication (Element-wise):")
+print(C_mul)
+
+print("\nMatrix Multiplication (Dot Product):")
+print(C_dot)
