78f256b48d0e5e5c5d2eb3dcd795dda5b09bc99f
spinythyme34 <spinythyme05@gmail.com>
2024-12-12 01:54:03 +0530

    Create matrix.py

---
+++ matrix.py
@@ -0,0 +1,16 @@
+import numpy as py
+A=py.array([[1,2,3],[4,5,6],[7,8,9]])
+B=py.array([[9,8,7],[6,5,4],[3,2,1]])
+result=py.add(A,B)
+print(result)
+
+import numpy as py
+A=py.array([[1,2,3],[4,5,6],[7,8,9]])
+B=py.array([[9,8,7],[6,5,4],[3,2,1]])
+print(A-B)
+
+import numpy as py
+A=py.array([[1,2,3],[4,5,6],[7,8,9]])
+B=py.array([[9,8,7],[6,5,4],[3,2,1]])
+result=py.dot(A,B)
+print(result)
